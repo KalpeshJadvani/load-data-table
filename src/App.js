1eb0dd02ae151b250ef46e51bb72d030e0fb19f2
mac-kalpesh <mac-kalpesh@kalpehsMac.local>
2020-01-10 20:16:09 +0530

    Added sorting by name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Table from './components/Table';
+
+const DESC = 'desc';
+const ASC = 'asc';
 
 class App extends Component {
   constructor(props) {
@@ -11,16 +14,21 @@ class App extends Component {
       error: false,
       page: 1,
       limit: 5,
-      total: 0
+      total: 0,
+      order: ASC
     };
     this.onShowSizeChange = this.onShowSizeChange.bind(this);
     this.onchange = this.onchange.bind(this);
+    this.onOrderChange = this.onOrderChange.bind(this);
   }
   loadData() {
-    const { page, limit } = this.state;
+    const { page, limit, order } = this.state;
     let mTotal = 0;
     this.setState({ isLoading: true });
-    fetch(`/api/shipments?_page=${page}&_limit=${limit}`)
+
+    fetch(
+      `/api/shipments?_page=${page}&_limit=${limit}&_sort=name&_order=${order}`
+    )
       .then(result => {
         for (let entry of result.headers.entries()) {
           if (entry[0] === 'x-total-count') {
@@ -30,6 +38,7 @@ class App extends Component {
         return result.json();
       })
       .then(result => {
+        console.log(result);
         this.setState({
           isLoading: false,
           shipments: result,
@@ -45,7 +54,10 @@ class App extends Component {
   }
 
   onShowSizeChange = (e, size) => {
-    this.setState({ limit: size }, () => {
+    const { total, page } = this.state;
+    const maxNoOfPage = Math.ceil(total / size);
+    const nPage = Math.min(maxNoOfPage, page);
+    this.setState({ limit: size, page: nPage }, () => {
       this.loadData();
     });
     console.log('size', size);
@@ -61,8 +73,23 @@ class App extends Component {
     this.loadData();
   }
 
+  onOrderChange = () => {
+    const { order } = this.state;
+    this.setState({ order: order === ASC ? DESC : ASC }, () => {
+      this.loadData();
+    });
+  };
+
   render() {
-    const { isLoading, shipments, error, total, page, limit } = this.state;
+    const {
+      isLoading,
+      shipments,
+      error,
+      total,
+      page,
+      limit,
+      order
+    } = this.state;
 
     if (isLoading) {
       return <div className="App">Loading...</div>;
@@ -73,14 +100,19 @@ class App extends Component {
     }
 
     return (
-      <Table
-        onShowSizeChange={this.onShowSizeChange}
-        limit={limit}
-        shipments={shipments}
-        total={total}
-        page={page}
-        onchange={this.onchange}
-      ></Table>
+      <Fragment>
+        <button onClick={this.onOrderChange}>
+          Sort by name: {order === ASC ? DESC : ASC}
+        </button>
+        <Table
+          onShowSizeChange={this.onShowSizeChange}
+          limit={limit}
+          shipments={shipments}
+          total={total}
+          page={page}
+          onchange={this.onchange}
+        />
+      </Fragment>
     );
   }
 }
